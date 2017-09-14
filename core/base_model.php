9b9bf71640f2dfb12403d307d7f0aa8dbedb9f45
noel <toyonoel@gmail.com>
2017-09-14 14:41:38 -0400

    [ADD] las funciones obtener,eliminar y actualizar en el model

--- core/base_model.php
+++ core/base_model.php
@@ -67,4 +67,56 @@ class BaseModel
 			die($e->getMessage());
 		}
 	}
+
+    public function eliminar(int $id){
+        try{
+            $stm = $this->pdo->prepare("DELETE FROM :table WHERE id = :id");
+            $stm->bindParam(":table", $this->getTable(), PDO::PARAM_STR);
+            $stm->bindParam(":id", $id, PDO::PARAM_INT);
+            return $stm->execute();
+
+        }
+        catch(Exception $e){
+            die($e->getMessage());
+        }
+
+    }
+
+    public function actualizar(int $id, array $valores){
+        try {
+            
+            $sql = "UPDATE";
+            $sql .= $this->getTable();
+            $sql .= " SET ";
+
+
+            foreach ($valores as $clave => $valor){
+
+                $sql .= $clave." = ";
+                $sql .= $valor.","
+            }
+            $sql = rtrim($sql, ",")
+            $sql .= " WHERE id= ".$id
+
+            $this->pdo->prepare($sql)->execute();
+        
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+
+    }
+
+    public function obtener(int $id){
+        try{
+            $stm = $this->pdo->prepare("SELECT * FROM :table WHERE id = :id");
+            $stm->bindParam(":table", $this->getTable(), PDO::PARAM_STR);
+            $stm->bindParam(":id", $id, PDO::PARAM_INT);
+            $stm->execute();
+            return $stm->fetch(PDO::FETCH_OBJ);
+
+        }
+        catch(Exception $e){
+            die($e->getMessage());
+        }
+    }
 }
