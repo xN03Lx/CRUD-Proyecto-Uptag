c921cd37f0ab28de9af3d9136317daff3428bbbd
noel <toyonoel@gmail.com>
2017-09-24 13:28:09 -0400

    [MOD] nueva bd

--- core/database.php
+++ core/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=proyecto_pnfi;charset=utf8', 'root', 'canaima');
+        $pdo = new PDO('mysql:host=localhost;dbname=control_registro_pnfi;charset=utf8', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
